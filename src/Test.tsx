d8ba5ca74f6b9716b823e90c60defd6721b731e9
trevorXu <xymzhs@gmail.com>
2022-06-10 15:00:21 +0800

    feat : involve webgpu

--- src/Test.tsx
+++ src/Test.tsx
@@ -1,19 +1,20 @@
 import { useEffect } from "react";
-import { add, dot, randn } from "../lib/array";
-import { linear } from "../lib/layer";
-import { tensor } from "../lib/tensor";
-const m = linear(2, 1, false);
-const input = randn([1, 2]);
-const y = m(input);
+import "../lib/gpu";
+// import { add, dot, randn } from "../lib/array";
+// import { linear } from "../lib/layer";
+// import { tensor } from "../lib/tensor";
+// const m = linear(2, 1, false);
+// const input = randn([1, 2]);
+// const y = m(input);
 
-const b = tensor([1, 2]);
-const c = tensor([3, 4]);
-console.log(b[1]);
-console.log(b());
-b.add(c);
-console.log("after", b());
+// const b = tensor([1, 2]);
+// const c = tensor([3, 4]);
+// console.log(b[1]);
+// console.log(b());
+// b.add(c);
+// console.log("after", b());
 
-console.log(c());
+// console.log(c());
 
 export function Test() {
   useEffect(() => {}, []);
--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,11 @@ import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),  dts()],
+  plugins: [react(), dts()],
   build: {
     lib: {
       entry: path.resolve(__dirname, "lib/index.ts"),
       name: "torch",
     },
-    
   },
 });
