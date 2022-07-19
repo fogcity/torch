13a4b59df5224c03fd8c0953ee3abea0b559c707
trevorXu <xymzhs@gmail.com>
2022-07-19 17:53:43 +0800

    feat : change wgsl

--- lib/gpu.ts
+++ lib/gpu.ts
@@ -1,3 +1,4 @@
+import co from "./shaders/compute.wgsl?raw";
 (async () => {
   if (!("gpu" in navigator)) {
     console.log(
@@ -22,10 +23,10 @@
   const gpuBufferFirstMatrix = device.createBuffer({
     mappedAtCreation: true,
     size: firstMatrix.byteLength,
-    usage: GPUBufferUsage.STORAGE,
+    usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
   });
-  const arrayBufferFirstMatrix = gpuBufferFirstMatrix.getMappedRange();
-  new Float32Array(arrayBufferFirstMatrix).set(firstMatrix);
+
+  new Float32Array(gpuBufferFirstMatrix.getMappedRange()).set(firstMatrix);
   gpuBufferFirstMatrix.unmap();
 
   // Second Matrix
@@ -37,55 +38,27 @@
   const gpuBufferSecondMatrix = device.createBuffer({
     mappedAtCreation: true,
     size: secondMatrix.byteLength,
-    usage: GPUBufferUsage.STORAGE,
+    usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
   });
-  const arrayBufferSecondMatrix = gpuBufferSecondMatrix.getMappedRange();
-  new Float32Array(arrayBufferSecondMatrix).set(secondMatrix);
+
+  new Float32Array(gpuBufferSecondMatrix.getMappedRange()).set(secondMatrix);
   gpuBufferSecondMatrix.unmap();
 
   // Result Matrix
 
   const resultMatrixBufferSize =
     Float32Array.BYTES_PER_ELEMENT * (2 + firstMatrix[0] * secondMatrix[1]);
+
   const resultMatrixBuffer = device.createBuffer({
-    size: resultMatrixBufferSize,
-    usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_SRC,
+    size:
+      Float32Array.BYTES_PER_ELEMENT * (2 + firstMatrix[0] * secondMatrix[1]),
+    usage: GPUBufferUsage.MAP_WRITE | GPUBufferUsage.COPY_SRC,
   });
 
   // Compute shader code
 
   const shaderModule = device.createShaderModule({
-    code: `
-    struct Matrix {
-      size : vec2<f32>,
-      numbers: array<f32>,
-    }
-
-    @group(0) @binding(0) var<storage, read> firstMatrix : Matrix;
-    @group(0) @binding(1) var<storage, read> secondMatrix : Matrix;
-    @group(0) @binding(2) var<storage, write> resultMatrix : Matrix;
-
-    @stage(compute) @workgroup_size(8, 8)
-    fn main(@builtin(global_invocation_id) global_id : vec3<u32>) {
-      // Guard against out-of-bounds work group sizes
-      if (global_id.x >= u32(firstMatrix.size.x) || global_id.y >= u32(secondMatrix.size.y)) {
-        return;
-      }
-
-      resultMatrix.size = vec2(firstMatrix.size.x, secondMatrix.size.y);
-
-      let resultCell = vec2(global_id.x, global_id.y);
-      var result = 0.0;
-      for (var i = 0u; i < u32(firstMatrix.size.y); i = i + 1u) {
-        let a = i + resultCell.x * u32(firstMatrix.size.y);
-        let b = resultCell.y + i * u32(secondMatrix.size.y);
-        result = result + firstMatrix.numbers[a] * secondMatrix.numbers[b];
-      }
-
-      let index = resultCell.y + resultCell.x * u32(secondMatrix.size.y);
-      resultMatrix.numbers[index] = result;
-    }
-    `,
+    code: co,
   });
 
   // Pipeline setup
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import dts from "vite-plugin-dts";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), dts()],
-
   build: {
     lib: {
       entry: path.resolve(__dirname, "lib/index.ts"),
